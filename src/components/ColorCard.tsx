2bd6540422e1ec5db18def620433820f4efb7a20
Jenya Shvartsman <jenyashvartsman@gmail.com>
2023-01-07 21:54:52 -0500

    color picker

--- src/components/ColorCard.tsx
+++ src/components/ColorCard.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useRef, useState } from "react";
+
 interface ColorCardProps {
   color: string;
   onUpdateColor: (newColor: string) => void;
 }
 
 export function ColorCard(props: ColorCardProps) {
+  const colorInputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="card" style={{ backgroundColor: props.color }}>
+      {/* color change */}
+      <button
+        className="card__button"
+        onClick={() => colorInputRef.current?.click()}
+      >
+        {props.color}
+      </button>
+
+      {/* color input */}
       <input
         type="color"
+        className="card__input"
+        ref={colorInputRef}
         value={props.color}
         onChange={(e) => props.onUpdateColor(e.target.value)}
       />
